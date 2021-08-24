13440c87b5f6def17611d9a82f364a82362fa864
yashkhasbage25 <yashkhasbage25@gmail.com>
2021-08-25 00:27:15 +0530

    audio only flag

--- rha.py
+++ rha.py
@@ -35,6 +35,7 @@ def parse_args():
     parser.add_argument('--inpath', type=str, required=True, help='path to input video')
     parser.add_argument('--facepath', type=str, default="", help='path to anonymous (target) face, can be a static video or a image. If no facepath is provided, the face will be hided. Hence, this argument is optional.')
     parser.add_argument('--outpath', type=str, required=True, help='path to output video, should be a mp4 video')
+    parser.add_argument('--audio_only', action='store_true', help='specify this option if the input is audio-only')
     parser.add_argument('--pitch', type=float, default=3, help='pitch change amount, can be +/-')
     parser.add_argument('--cpu_only', action='store_true', help='Run on cpu only. However this flag is only for swapper. Hider will use/not use gpu depending on the tensorflow type you have installed. For tensorflow you can have a gpu or a cpu version.')
     parser.add_argument('-pdb', action='store_true', help='run with pdb debugger')
@@ -56,99 +57,110 @@ if __name__ == '__main__':
     os.makedirs(temp_dir, exist_ok=True)
     inpath = osp.abspath(args.inpath)
     outpath = osp.abspath(args.outpath)
-    
-    metadata = ffmpeg.probe(inpath)
-    # right now, just works on 2 stream data. 
-    # will not work on audio-less videos which rarely occur
-    assert len(metadata) == 2, '2 streams are not found in input'
-    
-    # get video and audio meta data, if exeists
-    video_meta = None
-    audio_meta = None
-    for m in metadata['streams']:
-        if m['codec_type'] == 'audio':
-            audio_meta = m
-        elif m['codec_type'] == 'video':
-            video_meta = m
-        else:
-            raise Exception("unknown codec_type: {}".format(m['codec_type']))
-    
-    # get start times of audio and video, if exists
-    video_start = None
-    audio_start = None
-    assert (video_meta or audio_meta), 'neither video nor audio is present in input file'
-    if video_meta is not None:
-        video_start = float(video_meta['start_time'])
-    if audio_meta is not None:
-        audio_start = float(audio_meta['start_time'])
-    
-    video_start_arg = ""
-    audio_start_arg = ""
-    if video_start:
-        video_start_arg = f" -itsoffset {video_start} "
-    if audio_start:
-        audio_start_arg = f" -itsoffset {audio_start} "
+    if not args.audio_only:
+        metadata = ffmpeg.probe(inpath)
+        # right now, just works on 2 stream data. 
+        # will not work on audio-less videos which rarely occur
+        assert len(metadata) == 2, '2 streams are not found in input'
         
-    # use face swapper if --facepath is provided
-    if args.facepath:
-        swappy_path = osp.join(osp.dirname(__file__), 'fsgan', 'inference', 'swap.py')
-        assert osp.exists(swappy_path), f'path not found: {swappy_path}'
-        facepath = osp.abspath(args.facepath)
-        assert osp.exists(facepath), f'facepath does not exist {facepath}'
-        print("Swapping faces, with the face:", facepath)
-        print('Input video:', inpath)
-        device_flag = ""
-        if args.cpu_only:
-            device_flag = " --cpu_only "
-        # swap faces
-        fsgan_outpath = osp.join(temp_dir, 'fsgan_out.mp4')
-        command = f'python {swappy_path} {facepath} -t {inpath} -o {fsgan_outpath} --seg_remove_mouth --encoder_codec mp4v {device_flag}'
-        error = os.system(command)
-        if error:
-            raise Exception(f'unable to swap faces. Check fsgan. error code: {error}')
+        # get video and audio meta data, if exeists
+        video_meta = None
+        audio_meta = None
+        for m in metadata['streams']:
+            if m['codec_type'] == 'audio':
+                audio_meta = m
+            elif m['codec_type'] == 'video':
+                video_meta = m
+            else:
+                raise Exception("unknown codec_type: {}".format(m['codec_type']))
         
-        visually_anon_output = fsgan_outpath
-    else:    
-        # use face hider if --facepath is not provided
-        print("Hiding the face, as the facepath argument was empty")
-        visually_anon_output = osp.join(temp_dir, "hidden_face.mp4")
-        hide_face_py_path = osp.join(osp.dirname(__file__), 'hide_face_robust.py')
-        assert osp.exists(hide_face_py_path), f"file not found: {hide_face_py_path}"
-        command = f"python {hide_face_py_path} --inpath {inpath} --outpath {visually_anon_output}"
-        error = os.system(command)
-        if error:
-            raise Exception(f"unable to run face hider. Check hide_face_robust.py. error code: {error}")
-    
-    # save the intermediate audio files as .wav
-    audcodec = 'wav'
-    tmpaudiopath1 = osp.join(temp_dir, f'aud1.{audcodec}')  
-    tmpaudiopath2 = osp.join(temp_dir, f'aud2.{audcodec}')
-    
-    # extract audio
-    subprocess.call(
-        f"ffmpeg -y -i {inpath} -vn {tmpaudiopath2}",
-        shell=True
-    )
-    
-    # transform the audio
-    audio_py_path = osp.join(osp.dirname(__file__), 'audio.py')
-    assert osp.exists(audio_py_path), f"file not found: {audio_py_path}"
-    cmd = f"python {audio_py_path} --inpath {tmpaudiopath2} --outpath {tmpaudiopath1} --tr pitch --pitch_n_semitones {args.pitch}"
-    error = os.system(
-        cmd
-    )
-    if error:
-        raise Exception(f"unable to change audio. Check audio.py. error code: {error}")
-    
-    # combine anonymized audio and video
-    cmd = f"ffmpeg -y {video_start_arg} -i {visually_anon_output} {audio_start_arg} -i {tmpaudiopath1} -vcodec copy -acodec aac -map 0:v:0 -map 1:a:0 {outpath}"
-    print('combining audio and video with command:', cmd)
-    subprocess.call(
-        cmd,
-        shell=True
-    )
+        # get start times of audio and video, if exists
+        video_start = None
+        audio_start = None
+        assert (video_meta or audio_meta), 'neither video nor audio is present in input file'
+        if video_meta is not None:
+            video_start = float(video_meta['start_time'])
+        if audio_meta is not None:
+            audio_start = float(audio_meta['start_time'])
+        
+        video_start_arg = ""
+        audio_start_arg = ""
+        if video_start:
+            video_start_arg = f" -itsoffset {video_start} "
+        if audio_start:
+            audio_start_arg = f" -itsoffset {audio_start} "
+            
+        # use face swapper if --facepath is provided
+        if args.facepath:
+            swappy_path = osp.join(osp.dirname(__file__), 'fsgan', 'inference', 'swap.py')
+            assert osp.exists(swappy_path), f'path not found: {swappy_path}'
+            facepath = osp.abspath(args.facepath)
+            assert osp.exists(facepath), f'facepath does not exist {facepath}'
+            print("Swapping faces, with the face:", facepath)
+            print('Input video:', inpath)
+            device_flag = ""
+            if args.cpu_only:
+                device_flag = " --cpu_only "
+            # swap faces
+            fsgan_outpath = osp.join(temp_dir, 'fsgan_out.mp4')
+            command = f'python {swappy_path} {facepath} -t {inpath} -o {fsgan_outpath} --seg_remove_mouth --encoder_codec mp4v {device_flag}'
+            error = os.system(command)
+            if error:
+                raise Exception(f'unable to swap faces. Check fsgan. error code: {error}')
+            
+            visually_anon_output = fsgan_outpath
+        else:    
+            # use face hider if --facepath is not provided
+            print("Hiding the face, as the facepath argument was empty")
+            visually_anon_output = osp.join(temp_dir, "hidden_face.mp4")
+            hide_face_py_path = osp.join(osp.dirname(__file__), 'hide_face_robust.py')
+            assert osp.exists(hide_face_py_path), f"file not found: {hide_face_py_path}"
+            command = f"python {hide_face_py_path} --inpath {inpath} --outpath {visually_anon_output}"
+            error = os.system(command)
+            if error:
+                raise Exception(f"unable to run face hider. Check hide_face_robust.py. error code: {error}")
+        
+        # save the intermediate audio files as .wav
+        audcodec = 'wav'
+        tmpaudiopath1 = osp.join(temp_dir, f'aud1.{audcodec}')  
+        tmpaudiopath2 = osp.join(temp_dir, f'aud2.{audcodec}')
+        
+        # extract audio
+        subprocess.call(
+            f"ffmpeg -y -i {inpath} -vn {tmpaudiopath2}",
+            shell=True
+        )
 
-    # remove the temporary files
-    shutil.rmtree(temp_dir)
-    print('video saved at:', outpath)
-    
+        # transform the audio
+        audio_py_path = osp.join(osp.dirname(__file__), 'audio.py')
+        assert osp.exists(audio_py_path), f"file not found: {audio_py_path}"
+        cmd = f"python {audio_py_path} --inpath {tmpaudiopath2} --outpath {tmpaudiopath1} --tr pitch --pitch_n_semitones {args.pitch}"
+        error = os.system(
+            cmd
+        )
+        if error:
+            raise Exception(f"unable to change audio. Check audio.py. error code: {error}")
+        
+        # combine anonymized audio and video
+        cmd = f"ffmpeg -y {video_start_arg} -i {visually_anon_output} {audio_start_arg} -i {tmpaudiopath1} -vcodec copy -acodec aac -map 0:v:0 -map 1:a:0 {outpath}"
+        print('combining audio and video with command:', cmd)
+        subprocess.call(
+            cmd,
+            shell=True
+        )
+
+        # remove the temporary files
+        shutil.rmtree(temp_dir)
+        print('video saved at:', outpath)
+        
+    else:
+        # transform the audio
+        audio_py_path = osp.join(osp.dirname(__file__), 'audio.py')
+        assert osp.exists(audio_py_path), f"file not found: {audio_py_path}"
+        cmd = f"python {audio_py_path} --inpath {args.inpath} --outpath {args.outpath} --tr pitch --pitch_n_semitones {args.pitch}"
+        error = os.system(
+            cmd
+        )
+        if error:
+            raise Exception(f"unable to change audio. Check audio.py. error code: {error}")
+        print('audio saved at', args.outpath)
